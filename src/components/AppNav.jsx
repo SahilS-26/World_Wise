131c31dc630704589ffe4f7be3850ef98ab3c81f
Sahil Suthar <sahil26dd@gmail.com>
2024-10-16 23:29:52 +0530

    using css modules

---
+++ src/components/AppNav.jsx
@@ -0,0 +1,7 @@
+import styles from "./AppNav.module.css";
+
+function AppNav() {
+  return <div className={styles.nav}>App Nav</div>;
+}
+
+export default AppNav;
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,14 @@
+import { NavLink } from "react-router-dom";
 import PageNav from "../components/PageNav";
+import AppNav from "../components/AppNav";
 
 function Homepage() {
   return (
     <div>
       <PageNav />
-
+      <AppNav />
       <h1>WorldWise</h1>
+      <NavLink to="/app">Click here for go to App</NavLink>
     </div>
   );
 }
