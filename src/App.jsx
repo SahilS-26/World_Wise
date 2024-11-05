23cc45b397c0004d5408376c1a324714a3144008
Sahil Suthar <sahil26dd@gmail.com>
2024-11-05 23:12:46 +0530

    Dynamic routes with URL parameters

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import PageNotFound from "./pages/PageNotFound";
 import { useEffect, useState } from "react";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
+import City from "./components/City";
 
 const BASE_URL = "http://localhost:9999";
 
@@ -49,6 +50,7 @@ function App() {
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
+          <Route path="cities/:id" element={<City />} />
           <Route
             path="countries"
             element={<CountryList cities={cities} isLoading={isLoading} />}
